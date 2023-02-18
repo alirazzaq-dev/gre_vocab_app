f6b329eeabd885f8a6fb75e1332d28234927d09e
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2023-02-18 10:01:41 +0500

    updates

--- data/words.ts
+++ data/words.ts
@@ -7,13 +7,25 @@ export type Word = {
   
 export const wordsData = [
     {
-      name: "Cogent",
-      desc: "clear, logical, and convincing",
-      example: "they put forward cogent arguments for British membership"
+        name: "Cogent",
+        desc: "clear, logical, and convincing",
+        example: "they put forward cogent arguments for British membership"
     },
     {
         name: "Bolster",
         desc: "support or strengthen",
         example: "the fall in interest rates is starting to bolster confidence"
-      }
+    },
+    {
+        name: "Vigilant",
+        desc: "keeping careful watch for possible danger or difficulties.",
+        example: "the burglar was spotted by vigilant neighbours"
+
+    },
+    {
+        name: "Implausible",
+        desc: "(of an argument or statement) not seeming reasonable or probable; failing to convince.",
+        example: "this is a blatantly implausible claim"
+    }
+
   ]
